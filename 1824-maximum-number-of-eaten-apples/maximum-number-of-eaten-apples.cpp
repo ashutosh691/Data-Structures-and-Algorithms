959eabc6c8bd2bbd0f56510a1694cb92a867da75
Ashutosh Upreti <ashutoshishunishu@gmail.com>
2026-02-03 20:22:03 +0530

    Time: 59 ms (67.64%) | Memory: 51.1 MB (67.80%) - LeetSync

--- 1824-maximum-number-of-eaten-apples/maximum-number-of-eaten-apples.cpp
+++ 1824-maximum-number-of-eaten-apples/maximum-number-of-eaten-apples.cpp
@@ -4,13 +4,14 @@ public:
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
         int n = apples.size(), day = 0, ans = 0;
         while(day < n || !pq.empty()) {
-            if(day < n && apples[day] > 0) pq.push({day + days[day], apples[day]});
+            if(day < n && apples[day] != 0) pq.push({day + days[day], apples[day]});
             while(!pq.empty() && pq.top().first <= day) pq.pop();
             if(!pq.empty()) {
-                auto curr = pq.top(); pq.pop();
+                auto pair = pq.top();
+                pq.pop();
                 ans++;
-                curr.second--;
-                if(curr.second > 0) pq.push(curr);
+                pair.second--;
+                if(pair.second > 0) pq.push(pair);
             }
             ++day;
         }
