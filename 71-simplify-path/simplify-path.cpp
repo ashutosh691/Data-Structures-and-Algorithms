f32575a74ee82353a2fe953ff1e71ba851512a5d
Ashutosh Upreti <ashutoshishunishu@gmail.com>
2026-02-06 21:38:41 +0530

    Time: 4 ms (45.55%) | Memory: 15.1 MB (23.45%) - LeetSync

---
+++ 71-simplify-path/simplify-path.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        for(int i = 0; i < path.size(); ++i) {
+            if(path[i] == '/') continue;
+            string temp = "";
+            while(i < path.size() && path[i] != '/') temp += path[i++];
+            if(temp == ".") continue;
+            if(temp == "..") { 
+                if(!st.empty()) 
+                    st.pop(); 
+            }
+            else st.push(temp);
+        }
+        string res = "";
+        while(!st.empty()) { 
+            res = "/" + st.top() + res; 
+            st.pop(); 
+        }
+        return res.size() == 0 ? "/" : res;
+    }
+};
